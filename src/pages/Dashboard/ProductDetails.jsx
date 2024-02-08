1057eb26ffd03fbe4cfb65da77a0aa0a4663b5e3
Saroj Kumar Pandey <saroj425@gmail.com>
2024-02-08 12:32:02 +0530

    add to cart button added

--- src/pages/Dashboard/ProductDetails.jsx
+++ src/pages/Dashboard/ProductDetails.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
+import Navbarmenu from '../../components/Navbarmenu'
+import {Container,Row,Col,Image} from 'react-bootstrap';
 
 const ProductDetails = () => {
     const [details,setDetails] = useState();
+    const [cartvalue,setCartValue] = useState(1);
     const {id} = useParams();
     console.log("ididid",id)
     const getProdDetails = async()=>{
         const res = await axios.get(`https://fakestoreapi.com/products/${id}`);
         setDetails(res.data);
+    }
+    const addtoCart=()=>{
+        console.log("cart Value");
     }
     useEffect(()=>{
         getProdDetails();
@@ -16,11 +22,31 @@ const ProductDetails = () => {
     console.log("detailsdetailsdetails",details);
   return (
     <div>
-        {
-            details && details.map && details.map((det)=>(
-                <p>{det.title}</p>
-            ))
-        }
+        <Navbarmenu props={cartvalue}/>
+        <Container>
+            <Row className='d-flex'>
+                <Col md="6">                    
+                    <img src={details ? details.image :""} alt="" width={400}/>
+                </Col>
+                <Col md="6">
+                    <h2 className='mb-3 mt-5'>{details ? details.title :""}</h2>
+                    <p className='mb-3'>{details ? details.category : ""}</p>
+                    <span className=''><b>RS. {details ? details.price : ""}</b>&nbsp;&nbsp;&nbsp;&nbsp;<b>Rating :{details ? details.rating.rate : ""}</b></span>
+                    <p className=''>{details ? details.description : ""}</p>
+                    <div>
+                        <form className='d-flex'>
+                            <select  onChange={(e)=>setCartValue(e.target.value)}>
+                                <option value={1}>1</option>
+                                <option value={2}>2</option>
+                                <option value={3}>3</option>
+                                <option value={4}>4</option>
+                            </select>
+                            <button className='button sub-signup' style={{borderRadius:"0 !important" ,width:"200px"}} onClick={addtoCart}>Add to Cart</button>
+                        </form>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     </div>
   )
 }
